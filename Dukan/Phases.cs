71ab47fc7ec7e557c2e332d4f01ac13e98813ae7
KananaThund <kananykin@gmail.com>
2015-10-15 22:42:25 +0300

    First Git push
    First cahnges

---
+++ Dukan/Phases.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukan
+{
+    class Phases
+    {
+        private string name;
+        private string[] categories;
+        private string[,] descriptions;
+        private string description;
+
+
+        public Phases(string name, string[] categories, string[,] descriptions)
+        {
+            this.name = name;
+            this.categories = categories;
+            this.descriptions = descriptions;
+        }
+
+        public Phases (string name, string desciption)
+        {
+            this.name = name;
+            this.description = desciption;
+        }
+    }
+}
